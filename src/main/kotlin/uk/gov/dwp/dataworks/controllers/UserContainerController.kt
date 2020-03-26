56ce48cdd5b2509c09e4d34057f255901357379c
MattToczek <toczek.matt@gmail.com>
2020-03-26 15:00:02 +0000

    adding post function on UserContinerController to receive json payload from front end service

--- src/main/kotlin/uk/gov/dwp/dataworks/controllers/UserContainerController.kt
+++ src/main/kotlin/uk/gov/dwp/dataworks/controllers/UserContainerController.kt
@@ -1,19 +1,29 @@
 package uk.gov.dwp.dataworks.controllers
 
+import jdk.jfr.ContentType
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
+import uk.gov.dwp.dataworks.entity.UserRequestFramework
 import uk.gov.dwp.dataworks.services.ConfigKey
 import uk.gov.dwp.dataworks.services.ConfigurationService
+import uk.gov.dwp.dataworks.services.TaskDeploymentService
+import java.awt.PageAttributes
 
 @RestController
 class UserContainerController {
     @Autowired
+    lateinit var taskDeploymentService: TaskDeploymentService
+
+    @Autowired
     lateinit var configService: ConfigurationService
 
-    @GetMapping("/hello")
-    fun hello(): String {
-        return "hello!"
+    @PostMapping("/fesreq")
+    fun launchTask(@RequestBody requestBody: UserRequestFramework){
+        taskDeploymentService.taskDefinitionWithOverride(requestBody.getEcs_cluster_name(), requestBody.getEmr_cluster_host_name(), requestBody.getUser_name())
     }
 
 }
---
+++ src/main/kotlin/uk/gov/dwp/dataworks/entity/UserRequestFramework.kt
@@ -0,0 +1,26 @@
+package uk.gov.dwp.dataworks.entity
+
+class UserRequestFramework {
+    private lateinit var ecs_cluster_name: String
+    private lateinit var user_name: String
+    private lateinit var emr_cluster_host_name: String
+
+    fun getEcs_cluster_name(): String{
+        return ecs_cluster_name
+    }
+    fun setEcs_cluster_name(ecs_cluster_name: String){
+        this.ecs_cluster_name = ecs_cluster_name
+    }
+    fun getUser_name(): String{
+        return user_name
+    }
+    fun setUser_name(user_name: String){
+        this.user_name = user_name
+    }
+    fun getEmr_cluster_host_name(): String{
+        return emr_cluster_host_name
+    }
+    fun setEmr_cluster_host_name(emr_cluster_id: String){
+        this.emr_cluster_host_name = emr_cluster_id
+    }
+}
