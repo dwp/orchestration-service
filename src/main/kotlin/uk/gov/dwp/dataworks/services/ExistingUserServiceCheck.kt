85f37ce334a77209ad7d386b628e0226efa5dc67
Ben F <benj-fletch@users.noreply.github.com>
2020-04-15 16:40:52 +0100

    DW-3646 - Updating Tests & mocking spring

--- src/main/kotlin/uk/gov/dwp/dataworks/services/ExistingUserServiceCheck.kt
+++ src/main/kotlin/uk/gov/dwp/dataworks/services/ExistingUserServiceCheck.kt
@@ -3,17 +3,21 @@ package uk.gov.dwp.dataworks.services
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import software.amazon.awssdk.services.ecs.EcsClient
+import software.amazon.awssdk.services.ecs.model.DescribeServicesRequest
+import software.amazon.awssdk.services.ecs.model.DescribeServicesResponse
 
 @Service
 class ExistingUserServiceCheck{
     @Autowired
-    private lateinit var ecsDescribeServicesCall: EcsDescribeServicesCall
-    @Autowired
     private lateinit var configService : ConfigurationService
 
     fun check(userName: String, ecsClusterName: String):Boolean{
-        val ecsClient = EcsClient.builder().region(configService.awsRegion).build()
-        val listOfService = ecsDescribeServicesCall.servicesResponse(ecsClient, ecsClusterName, userName).services()
+        val listOfService = servicesResponse(ecsClusterName, userName).services()
         return listOfService.map { it.status() }.any { it == "ACTIVE" }
+    }
+
+    fun servicesResponse(ecsClusterName: String, userName: String): DescribeServicesResponse {
+        val ecsClient = EcsClient.builder().region(configService.awsRegion).build()
+        return ecsClient.describeServices(DescribeServicesRequest.builder().cluster(ecsClusterName).services("${userName}-analytical-workspace").build())
     }
 }
--- src/test/kotlin/uk/gov/dwp/dataworks/services/ExistingUserServiceCheckTest.kt
+++ src/test/kotlin/uk/gov/dwp/dataworks/services/ExistingUserServiceCheckTest.kt
@@ -2,41 +2,36 @@ package uk.gov.dwp.dataworks.services
 
 import com.auth0.jwt.interfaces.DecodedJWT
 import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.doReturn
 import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.whenever
-import org.junit.Assert
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 import org.junit.jupiter.api.BeforeEach
 import org.junit.runner.RunWith
 import org.mockito.ArgumentMatchers.anyString
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.boot.test.mock.mockito.SpyBean
 import org.springframework.test.context.junit4.SpringRunner
-import org.springframework.test.web.servlet.MockMvc
-import software.amazon.awssdk.regions.Region
 import software.amazon.awssdk.services.ecs.model.DescribeServicesResponse
 import software.amazon.awssdk.services.ecs.model.Service
 
 @RunWith(SpringRunner::class)
 @WebMvcTest(ExistingUserServiceCheck::class)
-class ExistingUserServiceCheckTest{
-    @Autowired
-    private lateinit var mvc: MockMvc
-    @Autowired
+class ExistingUserServiceCheckTest {
+    @SpyBean
     private lateinit var existingUserServiceCheckForTest: ExistingUserServiceCheck
 
     @MockBean
-    private lateinit var ecsDescribeServicesCall: EcsDescribeServicesCall
-    @MockBean
     private lateinit var authService: AuthenticationService
+
     @MockBean
     private lateinit var configService: ConfigurationService
 
     @BeforeEach
     fun setup() {
         whenever(authService.validate(any())).thenReturn(mock<DecodedJWT>())
-        whenever(configService.awsRegion).thenReturn(Region.EU_WEST_2)
     }
 
     private val testUserName = "testUser"
@@ -49,20 +44,23 @@ class ExistingUserServiceCheckTest{
     }
 
     @Test
-    fun `Existing Service prevent duplicates from spinning up`(){
-        whenever(ecsDescribeServicesCall.servicesResponse(any(), anyString(), anyString())).thenReturn(createDescribeServiceResponse( "$testUserName-analytical-workspace", "ACTIVE"))
-        Assert.assertEquals(true, existingUserServiceCheckForTest.check(testUserName, "test"))
+    fun `Existing Service prevent duplicates from spinning up`() {
+        val fakeResponse = createDescribeServiceResponse("$testUserName-analytical-workspace", "ACTIVE")
+        doReturn(fakeResponse).whenever(existingUserServiceCheckForTest).servicesResponse(anyString(), anyString())
+        assertThat(existingUserServiceCheckForTest.check(testUserName, "test")).isTrue()
     }
 
     @Test
-    fun `Existing inactive service allows creation`(){
-        whenever(ecsDescribeServicesCall.servicesResponse(any(), anyString(), anyString())).thenReturn(createDescribeServiceResponse( "$testUserName-analytical-workspace", "INACTIVE"))
-        Assert.assertEquals(false, existingUserServiceCheckForTest.check(testUserName, "test"))
+    fun `Existing inactive service allows creation`() {
+        val fakeResponse = createDescribeServiceResponse("$testUserName-analytical-workspace", "INACTIVE")
+        doReturn(fakeResponse).whenever(existingUserServiceCheckForTest).servicesResponse(anyString(), anyString())
+        assertThat(existingUserServiceCheckForTest.check(testUserName, "test")).isFalse()
     }
 
     @Test
-    fun `No matching service allows creation`(){
-        whenever(ecsDescribeServicesCall.servicesResponse(any(), anyString(), anyString())).thenReturn(DescribeServicesResponse.builder().build())
-        Assert.assertEquals(false, existingUserServiceCheckForTest.check(testUserName, "test"))
+    fun `No matching service allows creation`() {
+        val fakeResponse = DescribeServicesResponse.builder().build()
+        doReturn(fakeResponse).whenever(existingUserServiceCheckForTest).servicesResponse(anyString(), anyString())
+        assertThat(existingUserServiceCheckForTest.check(testUserName, "test")).isFalse()
     }
 }
