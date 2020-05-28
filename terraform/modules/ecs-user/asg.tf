bdf540e429dda7fe129dedde590e017fe3039614
Casey Rogers <2818278+caseyr232@users.noreply.github.com>
2020-05-28 15:13:49 +0100

    DW-4176: Os Sec Reveiw

--- terraform/modules/ecs-user/asg.tf
+++ terraform/modules/ecs-user/asg.tf
@@ -22,12 +22,6 @@ resource "aws_autoscaling_group" "user_host" {
     }
   }
 
-  tag {
-    key                 = "Name"
-    value               = "${var.name_prefix}-asg"
-    propagate_at_launch = false
-  }
-
   lifecycle {
     create_before_destroy = true
   }
