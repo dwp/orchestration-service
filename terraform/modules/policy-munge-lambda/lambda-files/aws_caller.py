8e3066531754f2610db931547fcf34147e9bd766
MattToczek <toczek.matt@gmail.com>
2020-11-12 13:53:16 +0000

    refactor and commenting

--- terraform/modules/policy-munge-lambda/lambda-files/aws_caller.py
+++ terraform/modules/policy-munge-lambda/lambda-files/aws_caller.py
@@ -2,25 +2,31 @@ import boto3
 
 iam_client = boto3.client('iam')
 
+"""
+============================================================================================================
+=================================== Helper methods that interact with AWS ==================================
+============================================================================================================
+"""
+
+
+def list_all_policies_in_account():
+    policy_array = []
+    get_paginated_policies_using_marker(iam_client.list_policies(Scope='All'), policy_array)
+    return policy_array
+
 
 # list_policies() call is paginated - set a marker and continue recursively
-def allow_for_truncation(result, result_array):
-    result_array.extend(result['Policies'])
-    if (result['IsTruncated']):
-        res = iam_client.list_policies(Scope='All', Marker=result['Marker'])
-        result_array.extend(res['Policies'])
-        allow_for_truncation(res, result_array)
+def get_paginated_policies_using_marker(aws_api_reponse, policy_array):
+    policy_array.extend(aws_api_reponse['Policies'])
+    if (aws_api_reponse['IsTruncated']):
+        res = iam_client.list_policies(Scope='All', Marker=aws_api_reponse['Marker'])
+        policy_array.extend(res['Policies'])
+        get_paginated_policies_using_marker(res, policy_array)
     else:
-        return result_array
+        return policy_array
 
 
-def list_policies():
-    result_array = []
-    allow_for_truncation(iam_client.list_policies(Scope='All'), result_array)
-    return result_array
-
-
-def get_policy_statement(arn, default_version_id):
+def get_policy_statement_as_list(arn, default_version_id):
     policy_version = iam_client.get_policy_version(
         PolicyArn=arn,
         VersionId=default_version_id
@@ -28,7 +34,7 @@ def get_policy_statement(arn, default_version_id):
     return policy_version['PolicyVersion']['Document']['Statement']
 
 
-def create_policy_from_json(policy_name, json_document):
+def create_policy_from_json_and_return_arn(policy_name, json_document):
     policy = iam_client.create_policy(
         PolicyName=policy_name,
         PolicyDocument=json_document
@@ -38,7 +44,7 @@ def create_policy_from_json(policy_name, json_document):
     return policy_arn
 
 
-def remove_old_policy(policy_arn, role_name):
+def remove_policy_being_replaced(policy_arn, role_name):
     # removes from role
     iam_client.detach_role_policy(
         RoleName=role_name,
@@ -68,22 +74,23 @@ def wait_for_policy_to_exist(arn):
     )
 
 
-def allow_for_tags_truncation(result, result_array, role_name):
-    result_array.extend(result['Tags'])
-    if (result['IsTruncated']):
-        res = iam_client.list_role_tags(RoleName=role_name, Marker=result['Marker'])
-        result_array.extend(res['Policies'])
-        allow_for_tags_truncation(res, result_array, role_name)
-    else:
-        return result_array
-
-
-def get_role_tags(role_name):
+def get_all_role_tags(role_name):
     result = iam_client.list_role_tags(
         RoleName=role_name,
     )
     result_array = []
-    return allow_for_tags_truncation(result, result_array, role_name)
+    return get_paginated_role_tags_using_marker(result, result_array, role_name)
+
+
+# list_role_tags() call is paginated - set a marker and continue recursively
+def get_paginated_role_tags_using_marker(result, result_array, role_name):
+    result_array.extend(result['Tags'])
+    if (result['IsTruncated']):
+        res = iam_client.list_role_tags(RoleName=role_name, Marker=result['Marker'])
+        result_array.extend(res['Policies'])
+        get_paginated_role_tags_using_marker(res, result_array, role_name)
+    else:
+        return result_array
 
 
 def tag_role(role_name, tag_name, tag_value):
--- terraform/modules/policy-munge-lambda/lambda-files/lambda_handler.py
+++ terraform/modules/policy-munge-lambda/lambda-files/lambda_handler.py
@@ -1,118 +1,167 @@
 import json
 import copy
-import os
 import re
 
-from aws_caller import list_policies, get_policy_statement, create_policy_from_json, attach_policy_to_role, \
-    remove_old_policy, tag_role, get_role_tags, delete_role_tags
+from aws_caller import list_all_policies_in_account, get_policy_statement_as_list, \
+    create_policy_from_json_and_return_arn, attach_policy_to_role, \
+    remove_policy_being_replaced, tag_role, get_all_role_tags, delete_role_tags
 
 # policy json template to be copied and amended as needed
 iam_template = {"Version": "2012-10-17", "Statement": []}
 
+"""
+============================================================================================================
+========================================== Policy Munging Lambda ===========================================
 
-# checks original input against map used for policy
-def verify_policies(names, map_of_names_to_policies):
-    for policy_name in names:
-        if (not map_of_names_to_policies[policy_name]):
-            raise Exception(f"Policy called: {policy_name} not found in map.")
+The lambda is used to munge policies together, in order to allow larger numbers of policies to be assigned 
+to a single IAM role than would otherwise be possible.
+
+Inputs: 
+ - user name/role name: the lambda expects an existing IAM role with the same name as the user and this 
+ should be passed in as a root value within the event under the 'userName' key
+ - policy array: an array of existing policy names that are to have their permissions combined key: 'policyArray'.
+ 
+ Process: 
+ - All existing AWS policies are collected into a list, the list is filtered to find the policies in the 
+ input array.
+ - The lambda sets up an array of objects, one for each policy name provided in the policy array input:
+ [
+    {
+        "policy_name": <String: name of existing policy>,
+        "statement": <Array/List: JSON IAM Statment objects>,
+        "chars": <Int: number of chars in the statement JSON>,
+        "chunk_number": <Int: number relating to which policy this object will be munged into>
+    }, 
+    ...
+ ]
+ - The policy statements are munged into a new JSON policy, up to the max char limit dictated by AWS. If 
+ the policy reaches the limit, a new policy is started. The JSON is stored in a dict with its name as the key
+ - The policies are created
+ - The policies are attached to the existing role passed in as an input.
+ - The role is tagged with the names of the input policies for visability - the tags undergo a similar 
+ process to the policy JSON to ensure they don't hit their char limit. 
+ 
+============================================================================================================
+"""
+
+
+def lambda_handler(event, context):
+    user_name = event['userName']
+    policy_array = event['policyArray']
+
+    array_of_policy_objects = create_policy_object_array_from_policy_name_array(policy_array)
+
+    dict_of_policy_name_to_munged_policy_objects = chunk_policies_and_return_dict_of_policy_name_to_json(array_of_policy_objects, user_name)
+
+    remove_existing_user_policies(user_name)
+    list_of_policy_arns = create_policies_from_dict_and_return_list_of_policy_arns(dict_of_policy_name_to_munged_policy_objects)
+
+    attach_policies_to_role(list_of_policy_arns, user_name)
+    delete_tags(user_name)
+    tag_role_with_policies(policy_array, user_name)
+
+
+"""
+============================================================================================================
+======================================== Helper methods for handler ========================================
+============================================================================================================
+"""
 
 
 # gets list of all policies available then creates a map of policy name to statement json based on requested policies
-def get_policy_documents_by_name(names):
-    policy_name_to_document = {}
-    returned_policies = list_policies()
+def create_policy_object_array_from_policy_name_array(names):
+    policy_object_array = []
+    returned_policies = list_all_policies_in_account()
     for policy in returned_policies:
         if (policy['PolicyName'] in names):
-            document = get_policy_statement(policy['Arn'], policy['DefaultVersionId'])
-            policy_name_to_document[policy['PolicyName']] = document
-    verify_policies(names, policy_name_to_document)
-    return policy_name_to_document
+            statement = get_policy_statement_as_list(policy['Arn'], policy['DefaultVersionId'])
+            policy_object_array.append(
+                {
+                    'policy_name': policy['PolicyName'],
+                    'statement': statement,
+                    'chars': len(json.dumps(statement)),
+                    'chunk_number': None
+                }
+            )
+    verify_policies(names, policy_object_array)
+    return policy_object_array
+
+
+# checks original input against map used for policy
+def verify_policies(names, array_of_policy_objects):
+    if len(names) > len(array_of_policy_objects):
+        raise Exception("Policy missing from Map.")
+    # if(not policy['policy_name'] in array_of_policy_objects for policy in array_of_policy_objects):
+    #         raise Exception("Policy missing from Map.")
+
+
+# creates json of policy documents mapped to their policy name using iam_policy_template and statements
+# from existing policies.
+def chunk_policies_and_return_dict_of_policy_name_to_json(policy_object_array, user_name):
+    chars_in_empty_iam_template = 42
+    char_limit_of_json_policy = 6144
+    policy_object_array = assign_chunk_number_to_objects(policy_object_array, chars_in_empty_iam_template,
+                                                         char_limit_of_json_policy)
+    total_number_of_chunks = policy_object_array[(len(policy_object_array) - 1)]['chunk_number'] + 1
+    dict_of_policy_name_to_munged_policy_objects = {}
+    for policy in policy_object_array:
+        file_name = f'{user_name}-{policy["chunk_number"] + 1}of{total_number_of_chunks}'
+        if file_name in dict_of_policy_name_to_munged_policy_objects:
+            dict_of_policy_name_to_munged_policy_objects[file_name]['Statement'].extend(policy['statement'])
+        else:
+            iam_policy = copy.deepcopy(iam_template)
+            iam_policy['Statement'] = policy['statement']
+            dict_of_policy_name_to_munged_policy_objects[file_name] = iam_policy
+
+    # checks to see if 20 policy attachment limit is reached before creating policies
+    if (len(dict_of_policy_name_to_munged_policy_objects) > 20):
+        raise Exception(f"Maximum policy assignment exceeded for role: {user_name}")
+
+    return dict_of_policy_name_to_munged_policy_objects
+
+
+# fills chunk_number attribute of object based on AWS imposed character allowance
+def assign_chunk_number_to_objects(object_array, start_char, max_char):
+    count = 0
+    chars = start_char
+    print(f'this is tha policy object array: {object_array}')
+    for object in object_array:
+        print(f'this it the policy_object value: {object}')
+        if (chars + object['chars']) >= max_char:
+            count += 1
+            chars = start_char
+        object['chunk_number'] = count
+        chars += object['chars']
+        print(f'current chars: {chars}')
+    return object_array
 
 
 # deletes any existing policies made by the lambda to apply the fresh set to user
 def remove_existing_user_policies(user_name):
-    returned_policies = list_policies()
+    returned_policies = list_all_policies_in_account()
     regex = re.compile(f"{user_name}-\d*of\d*")
     for policy in returned_policies:
         if (regex.match(policy['PolicyName'])):
-            remove_old_policy(policy['Arn'], user_name)
-
-
-# gets length of statement json in chars
-def get_chars_in_document(policy_name_to_document):
-    policy_name_to_chars = {}
-    for policy in policy_name_to_document:
-        policy_name_to_chars[policy] = len(json.dumps(policy_name_to_document[policy]))
-    return policy_name_to_chars
-
-
-# creates map of count of files to produce to policy documents to add to each file
-def get_chunk_contents(policy_name_to_chars, start_char, max_char):
-    count = 1
-    # number of chars already in iam_template
-    # chars = 42
-    chars = start_char
-    dict_of_chunks = {count: []}
-    for policy in policy_name_to_chars:
-        # if ( (chars+policy_name_to_chars[policy]) >= 6144 ):
-        if ((chars + policy_name_to_chars[policy]) >= max_char):
-            count += 1
-            dict_of_chunks[count] = []
-            # chars = 42
-            chars = start_char
-        dict_of_chunks[count].append(policy)
-        chars += policy_name_to_chars[policy]
-        print(f'current chars: {chars}')
-        print(dict_of_chunks)
-    return dict_of_chunks
-
-
-# creates json files locally (/tmp/) that contain policy documents
-def chunk_policies(policy_name_to_document, policy_name_to_chars, user_name):
-    chunk_contents = get_chunk_contents(policy_name_to_chars, 42, 6144)
-    for chunk in chunk_contents:
-        file_name = f'{user_name}-{chunk}of{len(chunk_contents)}'
-        iam_policy = copy.deepcopy(iam_template)
-        for policy_name in chunk_contents[chunk]:
-            iam_policy['Statement'].extend(policy_name_to_document[policy_name])
-        with open(f'/tmp/{file_name}.json', 'a') as outfile:
-            json.dump(iam_policy, outfile)
+            remove_policy_being_replaced(policy['Arn'], user_name)
 
 
 # creates policies in IAM from JSON files and removes JSON files
-def create_policies_from_files(user_name):
-    arn_array = []
-    file_array = []
-    regex = re.compile(f"{user_name}-\d*of\d*\.json")
-    for file in os.listdir("/tmp/"):
-        if (regex.match(file)):
-            file_array.append(file)
-
-    # checks to see if 20 policy attachment limit is reached before creating policies
-    if (len(file_array) > 20):
-        raise Exception(f"Maximum policy assignment exceeded for role: {user_name}")
-
-    for file in file_array:
-        policy_name = file[:-5]
-        with open(f'/tmp/{file}') as f: json_document = f.read()
-        print(json_document)
-        policy_arn = create_policy_from_json(policy_name, json_document)
-        arn_array.append(policy_arn)
-        delete_local_file(f'/tmp/{file}')
-    return arn_array
+def create_policies_from_dict_and_return_list_of_policy_arns(dict_of_policy_name_to_munged_policy_objects):
+    list_of_policy_arns = []
+    for policy in dict_of_policy_name_to_munged_policy_objects:
+        policy_arn = create_policy_from_json_and_return_arn(policy, json.dumps(dict_of_policy_name_to_munged_policy_objects[policy]))
+        list_of_policy_arns.append(policy_arn)
+    return list_of_policy_arns
 
 
-def attach_policies_to_role(arn_array, user_name):
-    for arn in arn_array:
+def attach_policies_to_role(list_of_policy_arns, user_name):
+    for arn in list_of_policy_arns:
         attach_policy_to_role(arn, user_name)
 
 
-def delete_local_file(file):
-    os.remove(file)
-
 
 def delete_tags(role_name):
-    tags = get_role_tags(role_name)
+    tags = get_all_role_tags(role_name)
     regex = re.compile(f"InputPolicies-\d*of\d*")
     tag_name_array = []
     for tag in tags:
@@ -122,29 +171,35 @@ def delete_tags(role_name):
         delete_role_tags(tag_name_array, role_name)
 
 
+# creates tag values mapped to their tag name to avoid hitting the maximum tag per role
 def tag_role_with_policies(policy_array, role_name):
-    name_to_chars = {}
+    tag_object_array = []
     separator = '/'
+    chars_in_empty_tag = 0
+    char_limit_for_tag_value = 200
     for name in policy_array:
-        name_to_chars[name] = len(name)
-    chunked_tags = get_chunk_contents(name_to_chars, 0, 200)
-    for tag in chunked_tags:
-        value = separator.join(chunked_tags[tag])
-        tag_role(role_name, f'InputPolicies-{tag}of{len(chunked_tags)}', value)
+        tag_object_array.append(
+            {
+                "policy_name": name,
+                "chars": len(name),
+                "chunk_number": None
+            }
+        )
+    chunked_tag_object_array = assign_chunk_number_to_objects(tag_object_array,
+                                                              chars_in_empty_tag,
+                                                              char_limit_for_tag_value)
+    tag_keys_to_value_list = {}
+    total_number_of_chunks = chunked_tag_object_array[(len(chunked_tag_object_array) - 1)]['chunk_number'] + 1
+    for tag in chunked_tag_object_array:
+        tag_key = f'InputPolicies-{tag["chunk_number"] + 1}of{total_number_of_chunks}'
+        if tag_key in tag_keys_to_value_list:
+            tag_keys_to_value_list[tag_key].append(tag['policy_name'])
+        else:
+            tag_keys_to_value_list[tag_key] = [tag['policy_name']]
 
+    if len(tag_keys_to_value_list) > 50:
+        raise Exception("Tag limit for role exceeded")
 
-def lambda_handler(event, context):
-    user_name = event['userName']
-    policy_array = event['policyArray']
-
-    name_to_statement = get_policy_documents_by_name(policy_array)
-    name_to_chars = get_chars_in_document(name_to_statement)
-
-    chunk_policies(name_to_statement, name_to_chars, user_name)
-
-    remove_existing_user_policies(user_name)
-    arn_array = create_policies_from_files(user_name)
-
-    attach_policies_to_role(arn_array, user_name)
-    delete_tags(user_name)
-    tag_role_with_policies(policy_array, user_name)
+    for tag in tag_keys_to_value_list:
+        tag_value = separator.join(tag_keys_to_value_list[tag])
+        tag_role(role_name, tag, tag_value)
