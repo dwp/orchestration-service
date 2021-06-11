8932d38350b1edfd9552d61cc42efbabefa046c4
stevenburton <stevenburton@digital.uc.dwp.gov.uk>
2021-06-11 13:56:00 +0100

    Fix

--- terraform/modules/ecs-user/outputs.tf
+++ terraform/modules/ecs-user/outputs.tf
@@ -1,8 +1,10 @@
 output "outputs" {
   value = {
-    ecs_cluster              = {
-        arn = aws_ecs_cluster.user_host.arn
+    ecs_cluster = {
+      arn  = aws_ecs_cluster.user_host.arn
+      name = aws_ecs_cluster.user_host.name
     }
+    user_container_log_group = aws_cloudwatch_log_group.user_container_log_group.name
     security_group_id        = aws_security_group.user_host.id
   }
 }
