311fe067aa407c9cc381d1978f231f84082e56f6
ian.utley <ian.utley@infinityworks.com>
2021-06-24 11:49:00 +0100

    Improve unit test for connection response types

--- src/test/kotlin/uk/gov/dwp/dataworks/controllers/ConnectionControllerTest.kt
+++ src/test/kotlin/uk/gov/dwp/dataworks/controllers/ConnectionControllerTest.kt
@@ -9,6 +9,7 @@ import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.http.MediaType
 import org.springframework.test.context.junit4.SpringRunner
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
@@ -82,6 +83,7 @@ class ConnectionControllerTest {
                 .header("content-type", "application/json")
                 .header("Authorisation", "testBadToken"))
                 .andExpect(status().isUnauthorized)
+                .andExpect(content().contentType(MediaType.TEXT_PLAIN))
         mvc.perform(post("/disconnect")
                 .header("Authorisation", "testBadToken"))
                 .andExpect(status().isUnauthorized)
