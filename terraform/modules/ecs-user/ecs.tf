178eb5c9a3eafe7cf40594370e9db6a44cf05726
Connor Avery <ConnorAvery@digital.uc.dwp.gov.uk>
2021-08-27 15:10:09 +0100

    Remove CAP stratergy
    Signed-off-by: Connor Avery <ConnorAvery@digital.uc.dwp.gov.uk>

--- terraform/modules/ecs-user/ecs.tf
+++ terraform/modules/ecs-user/ecs.tf
@@ -4,10 +4,6 @@ resource "random_id" "capacity_provider_suffix" {
 
 resource "aws_ecs_cluster" "user_host" {
   name               = var.name_prefix
-  default_capacity_provider_strategy {
-    capacity_provider = aws_ecs_capacity_provider.user_host.name
-    weight            = 100
-  }
   tags = merge(var.common_tags, { Name = var.name_prefix, Persistence = "True", AutoShutdown = "False" })
 }
 
