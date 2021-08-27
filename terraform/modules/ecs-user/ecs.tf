50c34821035df7f156861ebcc4760ee683e9aa67
Connor Avery <ConnorAvery@digital.uc.dwp.gov.uk>
2021-08-27 16:51:33 +0100

    Decouple capacity provider
    Signed-off-by: Connor Avery <ConnorAvery@digital.uc.dwp.gov.uk>

--- terraform/modules/ecs-user/ecs.tf
+++ terraform/modules/ecs-user/ecs.tf
@@ -4,23 +4,24 @@ resource "random_id" "capacity_provider_suffix" {
 
 resource "aws_ecs_cluster" "user_host" {
   name               = var.name_prefix
+
   tags = merge(var.common_tags, { Name = var.name_prefix, Persistence = "True", AutoShutdown = "False" })
 }
 
-//resource "aws_ecs_capacity_provider" "user_host" {
-//  name = "${var.name_prefix}-${random_id.capacity_provider_suffix.hex}"
-//  auto_scaling_group_provider {
-//    auto_scaling_group_arn         = aws_autoscaling_group.user_host.arn
-//    managed_termination_protection = "ENABLED"
-//
-//    managed_scaling {
-//      status                    = "ENABLED"
-//      maximum_scaling_step_size = var.scaling.max
-//      minimum_scaling_step_size = var.scaling.step
-//      target_capacity           = 80
-//    }
-//
-//  }
-//  depends_on = [aws_autoscaling_group.user_host]
-//  tags       = merge(var.common_tags, { "Name" : "${var.name_prefix}-user-host-provider" })
-//}
+resource "aws_ecs_capacity_provider" "user_host" {
+  name = "${var.name_prefix}-${random_id.capacity_provider_suffix.hex}"
+  auto_scaling_group_provider {
+    auto_scaling_group_arn         = aws_autoscaling_group.user_host.arn
+    managed_termination_protection = "ENABLED"
+
+    managed_scaling {
+      status                    = "ENABLED"
+      maximum_scaling_step_size = var.scaling.max
+      minimum_scaling_step_size = var.scaling.step
+      target_capacity           = 80
+    }
+
+  }
+  depends_on = [aws_autoscaling_group.user_host]
+  tags       = merge(var.common_tags, { "Name" : "${var.name_prefix}-user-host-provider" })
+}
