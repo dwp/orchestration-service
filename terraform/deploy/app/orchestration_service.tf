ce3e6b1315a5c7239f46bdadedb4e6bec52f6703
Jonathan Melia <jonathan.melia@engineering.digital.dwp.gov.uk>
2023-06-19 11:00:21 +0100

    refactor: file locations to deploy

--- terraform/deploy/app/orchestration_service.tf
+++ terraform/deploy/app/orchestration_service.tf
@@ -296,10 +296,10 @@ module "ecs-user-host" {
   token                   = local.token
   policyid                = local.policy_id[local.environment]
   tanium_prefix           = local.tanium_prefix[local.environment]
-  config_bucket_id        = data.terraform_remote_state.management.outputs.config_bucket.id
-  config_bucket_arn       = data.terraform_remote_state.management.outputs.config_bucket.arn
-  config_bucket_cmk_arn   = data.terraform_remote_state.management.outputs.config_bucket.cmk_arn
-  s3_scripts_bucket       = data.terraform_remote_state.management.outputs.config_bucket.id
+  config_bucket_id        = data.terraform_remote_state.common.outputs.config_bucket.id
+  config_bucket_arn       = data.terraform_remote_state.common.outputs.config_bucket.arn
+  config_bucket_cmk_arn   = data.terraform_remote_state.common.outputs.config_bucket.cmk.arn
+  s3_scripts_bucket       = data.terraform_remote_state.common.outputs.config_bucket.id
 
   s3_packages = {
     bucket     = data.terraform_remote_state.common.outputs.config_bucket.id
