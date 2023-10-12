930ab4344dc4ecae60c53d94308a3a3d1cb4067a
sumit.bharadia <sumit.bharadia@engineering.digital.dwp.gov.uk>
2023-10-12 13:35:59 +0100

    test: tests new rstudio image built via gitlab-ci

--- terraform/deploy/app/variables.tf
+++ terraform/deploy/app/variables.tf
@@ -292,7 +292,7 @@ variable "component_tags" {
   default = {
     development = {
       hue             = "0.0.102"
-      rstudio_oss     = "0.0.59"
+      rstudio_oss     = "0.0.20"
       jupyter_hub     = "0.0.78"
       headless_chrome = "0.0.15"
       guacd           = "0.0.21"
